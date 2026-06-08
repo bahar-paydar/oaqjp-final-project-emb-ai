d0be91bca64e4ee73fa7ee59cebdf73583d12e62
bahar-paydar <bahar_paydar@proton.me>
2026-06-08 17:00:02 -0400

    error handling added

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,6 +2,16 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    if not text_to_analyze:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyze } }
--- server.py
+++ server.py
@@ -11,6 +11,8 @@ def index():
 def emotion_detector_api():
     text = request.args['textToAnalyze']
     result = emotion_detector(text)
+    if not result['dominant_emotion']:
+        return 'Invalid text! Please try again!'
     return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
     
 if __name__ == "__main__":
